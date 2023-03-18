1f1a9a3110cf9d3ae3b2108937b60bbed6e31b52
Sujit-Tiwari <128125082+Sujit-Tiwari@users.noreply.github.com>
2023-03-18 18:16:47 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -5,3 +5,5 @@ for(var i=0;i<items.length;i++)
     items[i].style.fontWeight="bold";
     items[i].style.color="red";
 }
+var lastitem=document.getElementsByTagName('li');
+lastitem[4].style.backgroundColor="gray"
